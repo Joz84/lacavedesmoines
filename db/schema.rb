b5830b96cecdd6a735e1f7f56721c0c94694f8ce
Yann Froment <yann.froment@yaneoweb.com>
2016-09-09 17:22:54 +0200

    plouf

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160909134728) do
+ActiveRecord::Schema.define(version: 20160909140209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,32 @@ ActiveRecord::Schema.define(version: 20160909134728) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "products", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "alcohol_id"
+    t.text     "description"
+    t.integer  "brewery_id"
+    t.integer  "region_id"
+    t.string   "country"
+    t.string   "capacity"
+    t.integer  "kind_id"
+    t.integer  "color_id"
+    t.integer  "fermentation_id"
+    t.float    "degree"
+    t.text     "specificity"
+    t.boolean  "returnable"
+    t.float    "promotion"
+    t.integer  "rating"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.index ["alcohol_id"], name: "index_products_on_alcohol_id", using: :btree
+    t.index ["brewery_id"], name: "index_products_on_brewery_id", using: :btree
+    t.index ["color_id"], name: "index_products_on_color_id", using: :btree
+    t.index ["fermentation_id"], name: "index_products_on_fermentation_id", using: :btree
+    t.index ["kind_id"], name: "index_products_on_kind_id", using: :btree
+    t.index ["region_id"], name: "index_products_on_region_id", using: :btree
+  end
+
   create_table "regions", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
@@ -68,4 +94,10 @@ ActiveRecord::Schema.define(version: 20160909134728) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "products", "alcohols"
+  add_foreign_key "products", "breweries"
+  add_foreign_key "products", "colors"
+  add_foreign_key "products", "fermentations"
+  add_foreign_key "products", "kinds"
+  add_foreign_key "products", "regions"
 end
