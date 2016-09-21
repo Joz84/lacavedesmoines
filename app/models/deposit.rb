449bf57658e4467148d787b0dd43cacdbc552a41
Jonathan Serafini <jonathan.serafini@gmail.com>
2016-09-21 15:01:06 +0200

    photos deposits

--- app/models/deposit.rb
+++ app/models/deposit.rb
@@ -5,4 +5,5 @@ class Deposit < ApplicationRecord
   validates :name, uniqueness: true
   validates :capacity, presence: true
   validates :capacity, numericality: { only_integer: true }
+  has_attachments :photos, maximum: 10
 end
