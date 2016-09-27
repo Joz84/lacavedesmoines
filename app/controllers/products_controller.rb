48260ceaad297c0491bd17dc259a9cc691ce63e1
Jonathan Serafini <jonathan.serafini@gmail.com>
2016-09-27 15:04:37 +0200

    rearch by color, kind and price boundaries

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,10 +2,17 @@ class ProductsController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    @products = Product.pgsearch(research_params["words"])
+#    if (  research_params["min_price"] && research_params["max_price"] && research_params["min_price"] > research_params["max_price"] )
+    words = (research_params.select { |k, v| !["min_price", "max_price"].include?(k)}.map { |k, v| v }).join(" ")
+    @products = !(words.strip).empty? ? Product.pgsearch(words) : Product.all
+    @products = @products.select { |p| (p.price_cents * 1.0 / 100) >= research_params["min_price"].to_f } if research_params["min_price"]
+    @products = @products.select { |p| (p.price_cents * 1.0 / 100) <= research_params["max_price"].to_f } if research_params["max_price"]
   end
 
   def show
+    @prices = Product.all.map {|p| p.price_cents * 1.0 / 100}.uniq.sort
+    @colors = Color.all.map { |c| c.name }
+    @kinds = Kind.all.map { |k| k.name }
     @product = Product.find(params[:id])
     @selection = Selection.new
     @suggestions = @product.matching(3)
@@ -14,7 +21,7 @@ class ProductsController < ApplicationController
   private
 
   def research_params
-    params.require(:research).permit(:words)
+    params.require(:research).permit(:words, [:color, :kind], :min_price, :max_price)
   end
 
 end
