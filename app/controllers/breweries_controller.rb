b883e1988b5f15742088007d95aa6540283e371a
Jonathan Serafini <jonathan.serafini@gmail.com>
2016-09-28 17:46:38 +0200

    fixed ptts pbs

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -1,5 +1,6 @@
 class BreweriesController < ApplicationController
   skip_before_action :authenticate_user!
+  before_action :lists_table, only: [:index, :show]
   def index
     @breweries = Brewery.all
   end
@@ -8,4 +9,14 @@ class BreweriesController < ApplicationController
     @brewery = Brewery.find(params[:id])
     @products = Product.where(brewery: @brewery)
   end
+
+  private
+
+  def lists_table
+    @list_prices = Product.all.map {|p| p.price_cents * 1.0 / 100}.uniq.sort
+    @list_colors = Color.all.map { |c| c.name }
+    @list_kinds = Kind.all.map { |k| k.name }
+    @list_breweries = Brewery.all.map { |b| b.name }
+  end
+
 end
